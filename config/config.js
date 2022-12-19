dae0eb11c6cdf8ff2cf8c8ff238b1d11ef466a9d
Riccardo Reali <rreali@my.centennialcollege.ca>
2022-12-19 15:49:14 -0500

    portfolio update 3

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@
 
 export const Secret = "MySecret";
 //export const MongoURI  = "mongodb://127.0.0.1/media"//THIS IS A LOCAL MACHINE DATABASE
-export const MongoURI  = "mongodb+srv://riccardo91:riccardo123@cluster0.fhc4vha.mongodb.net/media1?retryWrites=true&w=majority"// ATLAS CONNECTION STRING
+export const MongoURI  = "mongodb+srv://riccardo91:riccardo123@cluster0.fhc4vha.mongodb.net/Portfolio?retryWrites=true&w=majority"// ATLAS CONNECTION STRING
 
 
 
